13a12f538607608ee2f3626fe30ac89481b10eba
tom <tom@getlimelite.com>
2020-11-13 12:19:56 -0500

    rack update

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,18 @@ module ApiModeDevise
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource(
+          '*',
+          headers: :any,
+          expose: ["Authorization"],
+          methods: [:get, :patch, :put, :delete, :post, :options,               :show]
+        )
+      end
+    end
+
     config.generators do |g|
       g.test_framework      :rspec, fixture_replacement: :fabrication
       g.fixture_replacement :fabrication, :dir => "spec/fabricators"
